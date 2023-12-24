2dc4a21469ba0c22d080128e8ff9242596183f1e
eason880913 <eason88091372104@gmail.com>
2023-12-24 23:04:47 +0800

    comprehession-lambda-handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,8 @@
-import pandas as pd
+import json
+
 
 def lambda_handler(event, context):
-    # 創建一個示例的DataFrame
-    data = {'姓名': ['小明', '小華', '小李', '小張'],
-            '年齡': [25, 30, 28, 22],
-            '城市': ['台北', '台中', '高雄', '新竹']}
-
-    df = pd.DataFrame(data)
-
-    # 顯示DataFrame的內容
-    print(df)
-    print('test0000002')
+    return {
+        'statusCode': 200,
+        'body': json.dumps(event)
+    }
