ab42efadb32c4b95221650ebb0397bc1af49c215
eason880913 <eason88091372104@gmail.com>
2023-12-24 16:37:35 +0800

    deploy-to-lambda-test

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # amazon-crawler
-d
---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def lambda_hander(event, context):
+    # 創建一個示例的DataFrame
+    data = {'姓名': ['小明', '小華', '小李', '小張'],
+            '年齡': [25, 30, 28, 22],
+            '城市': ['台北', '台中', '高雄', '新竹']}
+
+    df = pd.DataFrame(data)
+
+    # 顯示DataFrame的內容
+    print(df)
